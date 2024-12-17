d953a6c7f4de0fde48bad4c23bfee62aef35733f
Masegolex <160056223+Masegolex@users.noreply.github.com>
2024-12-17 18:50:15 +0200

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -5,26 +5,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://github.com/Masegolex"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          {" "}
-          Masego Ntuane{" "}
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/Masegolex/react-weather-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://github.com/Masegolex"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Masego Ntuane{" "}
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Masegolex/react-weather-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
