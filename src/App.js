e4e77a9835bee3676271cf6bc626c99dfc066203
Masegolex <160056223+Masegolex@users.noreply.github.com>
2024-12-17 18:25:01 +0200

    Added Git repo URL

--- src/App.js
+++ src/App.js
@@ -3,5 +3,28 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://github.com/Masegolex"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          {" "}
+          Masego Ntuane{" "}
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Masegolex/react-weather-app"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
